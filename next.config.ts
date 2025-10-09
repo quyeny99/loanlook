43ce350a9d07f157bf9dc17821e0b64f65cd5570
Son Nguyen <sonnha.it@y99.vn>
2025-10-09 07:00:45 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://apiy.y99.vn/:path*',
+      },
+    ]
+  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
