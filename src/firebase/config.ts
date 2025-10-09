700ad27989b295f9ab4481d5d86e7347062b2045
Son Nguyen <sonnha.it@y99.vn>
2025-10-09 06:47:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-222430457-b30d2",
+  "appId": "1:1002705691979:web:7f0ddc1aa50232abaab4ce",
+  "apiKey": "AIzaSyDmREntA15dlpWxXMlww_LrEIyjkqQQ-u8",
+  "authDomain": "studio-222430457-b30d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1002705691979"
+};
