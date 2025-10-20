d1ba472e97c0f0e6be9eae7f3976d149222da72a
Son Nguyen <sonnha.it@y99.vn>
2025-10-20 03:46:56 +0000

    trang date range report có số lượng của api internal entry bạn nên chuyể

--- src/components/reports/date-range/summary-cards.tsx
+++ src/components/reports/date-range/summary-cards.tsx
@@ -82,11 +82,14 @@ export default function SummaryCards({ fromDate, setFromDate, toDate, setToDate,
                 </CardHeader>
                 <CardContent>
                     <div className="flex items-center gap-2">
-                        <div className="flex items-center justify-center px-2 h-7 rounded-md bg-orange-500 text-white font-bold">{collectedAmount.count}</div>
                         <p className="text-2xl font-bold text-orange-500">{currencyFormatter.format(reportData.totalCollectedAmount)} ₫</p>
                     </div>
                     <div className="text-xs mt-2 space-y-1">
-                        <p>Total Repayment Amount: <span className="font-semibold">{currencyFormatter.format(reportData.totalRepaymentAmount)} ₫</span></p>
+                        <div className="flex items-center gap-2">
+                            Total Repayment Amount: 
+                            <div className="flex items-center justify-center px-2 h-7 rounded-md bg-orange-500 text-white font-bold">{collectedAmount.count}</div>
+                            <span className="font-semibold">{currencyFormatter.format(reportData.totalRepaymentAmount)} ₫</span>
+                        </div>
                         <p>Total Collected Service Fees: <span className="font-semibold">{currencyFormatter.format(collectedServiceFees)} ₫</span></p>
                     </div>
                 </CardContent>
